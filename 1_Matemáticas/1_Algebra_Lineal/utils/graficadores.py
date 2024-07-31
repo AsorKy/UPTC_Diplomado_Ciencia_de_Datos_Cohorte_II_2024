61cc55c16a31bdabe3420f8c18f616a019490a3d
Asor <asorfeienkurmetekeya@gmail.com>
2024-07-31 14:51:12 -0500

    Curso de algebra lineal finalizado

--- 1_Matemáticas/1_Algebra_Lineal/utils/graficadores.py
+++ 1_Matemáticas/1_Algebra_Lineal/utils/graficadores.py
@@ -240,3 +240,51 @@ def transformacion_de_espacio(A):
 
     # Mostrar el resultado
     plt.show()
+    
+
+#-------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------- Graficador de vectores ---------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------
+
+def graficarVectores(vecs,cols,alpha=1):
+  # we add two lines for the axis
+  plt.axvline(x = 0, color = 'grey', zorder = 0)
+  plt.axhline(y = 0, color = 'grey', zorder = 0)
+
+  for i in range(len(vecs)):
+    x = np.concatenate([[0,0], vecs[i]])
+    plt.quiver([x[0]],
+               [x[1]],
+               [x[2]],
+               [x[3]],
+               angles = 'xy', scale_units = 'xy',
+               scale = 1,
+               color = cols[i],
+               alpha = alpha)
+    
+ 
+#-------------------------------------------------------------------------------------------------------------------
+#----------------------------------- Graficador de Matrices como un espacio vectorial ------------------------------
+#-------------------------------------------------------------------------------------------------------------------  
+
+ 
+def graficarMatriz(matriz, vectorCol=['red','blue']):
+
+  # unit circle
+  x = np.linspace(-1,1,100000)
+  y = np.sqrt(1-(x**2))
+
+  # transpormed unit circle
+  x1 = matriz[0,0]*x + matriz[0,1]*y
+  y1 = matriz[1,0]*x + matriz[1,1]*y
+  x1_neg = matriz[0,0]*x - matriz[0,1]*y
+  y1_neg = matriz[1,0]*x - matriz[1,1]*y
+
+  # vectors
+  u1 = [matriz[0,0], matriz[1,0]]
+  v1 = [matriz[0,1], matriz[1,1]]
+
+  graficarVectores([u1, v1], cols=[vectorCol[0], vectorCol[1]])
+
+  plt.plot(x1,y1, 'green', alpha = 0.7)
+  plt.plot(x1_neg, y1_neg, 'green', alpha = 0.7)
