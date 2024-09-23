9c1b352ab0927e2dce15990b8a8606e25e326b3e
AsorKy <asorfeienkurmetekeya@gmail.com>
2024-09-22 23:44:17 -0500

    visualizacion de datos y SQL agregado

---
+++ 2_Analítica_Datos/2_Fundamentos_de_SQL/project/consultas_generales.sql
@@ -0,0 +1,20 @@
+select *
+from hospitals;
+
+select *
+from insurance_companies;
+
+select *
+from patients;
+
+select * 
+from prescriptions;
+
+select *
+from appointments;
+
+select *
+from physicians;
+
+select *
+from treatments;
